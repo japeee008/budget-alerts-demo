9380df21065c533cd377adca1d9785327295343e
Jeff <jefflloyd.seloterio@gmail.com>
2025-09-29 16:24:00 +0800

    chore(budget_alerts): wire app into settings and urls with health endpoint

--- budget_alerts/views.py
+++ budget_alerts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
+def health(request):
+    return JsonResponse({"ok": True, "feature": "budget_alerts"})
 # Create your views here.
