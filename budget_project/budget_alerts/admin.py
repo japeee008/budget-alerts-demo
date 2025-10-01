969db49f8735bcf57f50dd3b0c5e2f9ab393f430
Jeff <jefflloyd.seloterio@gmail.com>
2025-10-02 05:11:29 +0800

    feat(budget_alerts): add Category and BudgetAlert models

--- budget_project/budget_alerts/admin.py
+++ budget_project/budget_alerts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, BudgetAlert
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "user")
+    search_fields = ("name", "user__username")
+
+@admin.register(BudgetAlert)
+class BudgetAlertAdmin(admin.ModelAdmin):
+    list_display = ("category", "amount_limit", "threshold_percent", "active", "user", "created_at")
+    list_filter = ("active", "threshold_percent")
+    search_fields = ("category__name", "user__username")
+
 
 # Register your models here.
